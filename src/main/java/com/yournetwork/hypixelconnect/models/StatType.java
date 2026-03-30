79307fb7c73f9bccbe57a99e72c1146a4afc83d4
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-03-30 13:34:34 +0530

    Create StatType.java

---
+++ src/main/java/com/yournetwork/hypixelconnect/models/StatType.java
@@ -0,0 +1,6 @@
+package com.yournetwork.hypixelconnect.models;
+
+public enum StatType {
+    HEALTH, DEFENSE, STRENGTH, CRIT_CHANCE, CRIT_DAMAGE, 
+    ATTACK_SPEED, INTELLIGENCE, SPEED, MAGIC_FIND, PET_LUCK, TRUE_DEFENSE
+}
